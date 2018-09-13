1f0f448e0b3fb9f6c4a3ecbf8ecdeb9ac653f099
Michael Qvortrup <qm@zuehlke.com>
2018-09-13 10:19:11 +0200

    Single jar version

--- src/main/kotlin/qm/Hello.kt
+++ src/main/kotlin/qm/Hello.kt
@@ -12,7 +12,7 @@ class MyIntWrapper(val value:Int):Serializable
 fun main(args: Array<String>) {
     System.setProperty(SimpleLogger.LOG_FILE_KEY, "System.err")
 
-    val uuid = UUID(10,10)
+    val uuid = UUID.randomUUID()
     val map  = ChronicleMap
             .of(UUID::class.java, MyIntWrapper::class.java)
             .name("access-count-map")
@@ -23,13 +23,18 @@ fun main(args: Array<String>) {
 
     val app = Javalin.create().start(7000)
 
-    println(System.getProperty("java.io.tmpdir"))
+    val tempDir = System.getProperty("java.io.tmpdir")
 
     app.get("/") {
         ctx ->
         val count = map[uuid] ?: MyIntWrapper(0)
         map[uuid] = MyIntWrapper(count.value + 1)
-        ctx.result("Hello World: ${count.value}")
+        val result = """
+            |Hello World: ${count.value}
+            |java.io.tmpdir: ${tempDir}
+            |Keys: ${map.keys}
+        """.trimIndent()
+        ctx.result(result)
     }
 }
 
