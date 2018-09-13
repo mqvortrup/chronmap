c89476e8219d14a68951f35f5c75c11076f49881
Michael Qvortrup <qm@zuehlke.com>
2018-09-13 09:12:50 +0200

    Working version

--- src/main/kotlin/qm/Hello.kt
+++ src/main/kotlin/qm/Hello.kt
@@ -1,6 +1,35 @@
 package qm
 
+import io.javalin.Javalin
+import net.openhft.chronicle.map.ChronicleMap
+import org.slf4j.impl.SimpleLogger
+import java.io.File
+import java.io.Serializable
+import java.util.*
+
+class MyIntWrapper(val value:Int):Serializable
+
 fun main(args: Array<String>) {
-    println("Hello, World")
+    System.setProperty(SimpleLogger.LOG_FILE_KEY, "System.err")
+
+    val uuid = UUID(10,10)
+    val map  = ChronicleMap
+            .of(UUID::class.java, MyIntWrapper::class.java)
+            .name("access-count-map")
+            .averageKey(UUID(10, 10))
+            .averageValueSize(10.0)
+            .entries(10000)
+            .createOrRecoverPersistedTo(File(System.getProperty("java.io.tmpdir") + "access-count-map.dat"))
+
+    val app = Javalin.create().start(7000)
+
+    println(System.getProperty("java.io.tmpdir"))
+
+    app.get("/") {
+        ctx ->
+        val count = map[uuid] ?: MyIntWrapper(0)
+        map[uuid] = MyIntWrapper(count.value + 1)
+        ctx.result("Hello World: ${count.value}")
+    }
 }
 
