59e53a7e322b2c73c3c338c0499f50c1d89122e5
michael <qm@zuehlke.com>
2018-09-14 13:50:09 +0200

    Refactored with path separator

--- src/main/kotlin/qm/Hello.kt
+++ src/main/kotlin/qm/Hello.kt
@@ -10,35 +10,44 @@ import java.util.*
 class MyIntWrapper(val value:Int):Serializable
 
 fun main(args: Array<String>) {
-    System.setProperty(SimpleLogger.LOG_FILE_KEY, "System.err")
+    configureLogging()
 
-    val tempDir = System.getProperty("java.io.tmpdir")
+    val dataFileName = getDataFileName()
 
-    println(tempDir)
+    println(dataFileName)
 
     val uuid = UUID.randomUUID()
 
-    val map  = ChronicleMap
-            .of(UUID::class.java, MyIntWrapper::class.java)
-            .name("access-count-map")
-            .averageKey(UUID(10, 10))
-            .averageValueSize(10.0)
-            .entries(10000)
-            .createOrRecoverPersistedTo(File(tempDir + "/access-count-map.dat"))
+    val map  = createChronicleMap()
 
     val app = Javalin.create().start(7000)
-
 
     app.get("/") {
         ctx ->
         val count = map[uuid] ?: MyIntWrapper(0)
         map[uuid] = MyIntWrapper(count.value + 1)
         val result = """
-            |Hello World: ${count.value}
-            |java.io.tmpdir: ${tempDir}
-            |Keys: ${map.keys}
+            Hello World: ${count.value}
+            Data file name: ${dataFileName}
+            Keys: ${map.keys}
         """.trimIndent()
         ctx.result(result)
     }
 }
+
+fun configureLogging() {
+    System.setProperty(SimpleLogger.LOG_FILE_KEY, "System.err")
+}
+
+fun createChronicleMap(): ChronicleMap<UUID, MyIntWrapper> {
+    return ChronicleMap
+            .of(UUID::class.java, MyIntWrapper::class.java)
+            .name("access-count-map")
+            .averageKey(UUID(10, 10))
+            .averageValueSize(10.0)
+            .entries(10000)
+            .createOrRecoverPersistedTo(getDataFileName())
+}
+
+fun getDataFileName() = File("${System.getProperty("java.io.tmpdir")}${System.getProperty("file.separator")}access-count-map.dat")
 
